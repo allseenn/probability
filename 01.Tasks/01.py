7e11bade18a8f3ebbd20a94615a68bb3bf3465f9
allseen <allseen@live.ru>
2023-01-15 21:56:31 +0300

    01.Tasks

--- 01.Tasks/01.py
+++ 01.Tasks/01.py
@@ -1,7 +1,6 @@
 # Из колоды в 52 карты извлекаются случайным образом 4 карты. 
 # a) Найти вероятность того, что все карты – крести. 
 # б) Найти вероятность, что среди 4-х карт окажется хотя бы один туз.
-from math import factorial
 import numpy as np
 def combinations(n,k):
     return np.math.factorial(n) / (np.math.factorial(k) * np.math.factorial(n-k))
